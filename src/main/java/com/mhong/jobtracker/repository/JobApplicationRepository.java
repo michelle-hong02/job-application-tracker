f0ae382f38cadb7a8bc2a6eab97f80532514cac8
Michelle Hong <michelleyhong02@gmail.com>
2026-01-04 18:46:54 -0500

    created repository classes for JobApplication and UserRepository to be able to interact with the database

---
+++ src/main/java/com/mhong/jobtracker/repository/JobApplicationRepository.java
@@ -0,0 +1,15 @@
+package com.mhong.jobtracker.repository;
+
+import com.mhong.jobtracker.domain.JobApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
+    // save(entity) - saves entity
+    // findById(entity) - finds entity by ID, returns Optional<T>
+    // findAll(id) - returns a list of all entities
+    // delete(entity) - deletes an entity
+    // deleteById(id) - deletes by ID
+    // existsById(id) - checks if an entity exists
+}
