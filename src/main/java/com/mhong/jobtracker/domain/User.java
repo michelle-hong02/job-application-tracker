05f4f38904ddda324b15f59bc6cbd5b3bb5538a1
Michelle Hong <michelleyhong02@gmail.com>
2026-01-04 16:48:52 -0500

    set up psql docker container and updated the config

--- src/main/java/com/mhong/jobtracker/domain/User.java
+++ src/main/java/com/mhong/jobtracker/domain/User.java
@@ -1,11 +1,9 @@
 package com.mhong.jobtracker.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
