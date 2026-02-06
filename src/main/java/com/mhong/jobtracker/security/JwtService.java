4cd0cc73f6e9cc8b3ff7e50caaf1db839386269a
Michelle Hong <michelleyhong02@gmail.com>
2026-02-05 20:58:08 -0500

    using @Service instead of @Component annotation in JwtService and making sure jwt vars aren't nested in application.yaml

--- src/main/java/com/mhong/jobtracker/security/JwtService.java
+++ src/main/java/com/mhong/jobtracker/security/JwtService.java
@@ -7,7 +7,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
@@ -23,7 +23,7 @@ import java.util.function.Function;
  * - Validate token
  * - Extract username
  */
-@Component
+@Service
 public class JwtService {
 
     @Value("${jwt.secret}")
