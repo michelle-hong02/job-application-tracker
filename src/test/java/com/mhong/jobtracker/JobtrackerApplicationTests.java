b325404660f0303df372b14b9367d38a5342d6d9
Michelle Hong <michelleyhong02@gmail.com>
2026-05-10 16:59:22 -0700

    added a smoke test file

--- src/test/java/com/mhong/jobtracker/JobtrackerApplicationTests.java
+++ src/test/java/com/mhong/jobtracker/JobtrackerApplicationTests.java
@@ -3,11 +3,8 @@ package com.mhong.jobtracker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"JWT_SECRET=test-secret-for-tests"})
 class JobtrackerApplicationTests {
-
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
